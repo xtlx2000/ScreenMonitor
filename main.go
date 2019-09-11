920a4ad140667aa6a5163070eb8c2792669a6787
Pengcheng Lv <lvpengcheng6300@gmail.com>
2019-09-11 13:09:05 +0800

    a

--- main.go
+++ main.go
@@ -20,38 +20,49 @@ func save(img *image.RGBA, filePath string) {
 	png.Encode(file, img)
 }
 
+func run() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("panic")
+			fmt.Println(err)
+		}
+	}()
+
+	// Capture each displays.
+	n := screenshot.NumActiveDisplays()
+	if n <= 0 {
+		panic("Active display not found")
+	}
+
+	var all image.Rectangle = image.Rect(0, 0, 0, 0)
+
+	for i := 0; i < n; i++ {
+		bounds := screenshot.GetDisplayBounds(i)
+		all = bounds.Union(all)
+
+		_, err := screenshot.CaptureRect(bounds)
+		if err != nil {
+			fmt.Printf("err: %v", err)
+		}
+		fileName := fmt.Sprintf("%d_%dx%d.png", i, bounds.Dx(), bounds.Dy())
+		//save(img, fileName)
+
+		fmt.Printf("#%d : %v \"%s\"\n", i, bounds, fileName)
+	}
+
+	// Capture all desktop region into an image.
+	fmt.Printf("%v\n", all)
+	img, err := screenshot.Capture(all.Min.X, all.Min.Y, all.Dx(), all.Dy())
+	if err != nil {
+		panic(err)
+	}
+	save(img, "all.png")
+
+	time.Sleep(time.Second * 60 * 1)
+}
+
 func main() {
 	for {
-		// Capture each displays.
-		n := screenshot.NumActiveDisplays()
-		if n <= 0 {
-			panic("Active display not found")
-		}
-
-		var all image.Rectangle = image.Rect(0, 0, 0, 0)
-
-		for i := 0; i < n; i++ {
-			bounds := screenshot.GetDisplayBounds(i)
-			all = bounds.Union(all)
-
-			img, err := screenshot.CaptureRect(bounds)
-			if err != nil {
-				fmt.Printf("err: %v", err)
-			}
-			fileName := fmt.Sprintf("%d_%dx%d.png", i, bounds.Dx(), bounds.Dy())
-			save(img, fileName)
-
-			fmt.Printf("#%d : %v \"%s\"\n", i, bounds, fileName)
-		}
-
-		// Capture all desktop region into an image.
-		fmt.Printf("%v\n", all)
-		img, err := screenshot.Capture(all.Min.X, all.Min.Y, all.Dx(), all.Dy())
-		if err != nil {
-			panic(err)
-		}
-		save(img, "all.png")
-
-		time.Sleep(time.Second * 60 * 1)
+		run()
 	}
 }
